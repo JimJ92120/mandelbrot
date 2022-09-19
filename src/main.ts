4a7d716ed98f5e68320d5ff68c2649773e48158b
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-09-19 17:11:00 +0700

    feat(app): init Mandelbrot set and data and draw with Engine

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import init, * as wasm from "../dist/lib";
+import init, {
+  Mandelbrot,
+} from "../dist/lib";
+import Engine from "./engine";
 
-init()
-  .then(() => {
-    wasm.greet("WASM");
-  });
+window.addEventListener('load', () => {
+  init()
+    .then(() => {
+      const engine: Engine = new Engine("scene");
+      const maxIterationCount: number = 1000;
+      const mandelbrotSet = new Mandelbrot(
+        engine.dimension[0],
+        engine.dimension[1],
+        maxIterationCount
+      );
+
+      engine.draw(mandelbrotSet.data);
+    });
+});
